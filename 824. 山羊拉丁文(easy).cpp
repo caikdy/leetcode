b9db980f919fd3862a012262fe5b0baac8365e25
caikdy <caikedi576360403@gmail.com>
2022-04-21 10:19:16 +0800

    2022.04.21 feat 824. 山羊拉丁文(easy)
    Signed-off-by: caikdy <caikedi576360403@gmail.com>

---
+++ 824. 山羊拉丁文(easy).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+class Solution
+{
+public:
+    string toGoatLatin(string sentence)
+    {
+        int count = 1;
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        string res;
+        int len = sentence.length();
+        for (int i = 0, j = 0; j < len; j++)
+        {
+            while (len != sentence[j] != ' ')
+            {
+                j++;
+            }
+            count++;
+            if (count != 2)
+            {
+                res += ' ';
+            }
+            if (vowels.count(sentence[i]))
+            {
+                res += sentence.substr(i, j - i) + 'm' + string(count, 'a');
+            }
+            else
+            {
+                res+=sentence.substr(i+1,j-i-1)+sentence[i]+'m'+ string(count, 'a');
+            }
+        }
+    }
+};
